c0773ab10f9f42661c91f51e67e6105f53a45d1f
Attackzzw <49190204+Attackzzw@users.noreply.github.com>
2020-06-30 09:27:58 +0800

    Update CarRacingTask.py

--- CarRacingTask.py
+++ CarRacingTask.py
@@ -18,7 +18,7 @@ class Model(parl.Model):
         self.fc1 = layers.fc(size=hid1_size, act='tanh')
         self.fc2 = layers.fc(size=act_dim, act='softmax')
 
-    def forward(self, obs):  # 可直接用 model = Model(5); model(obs)调用
+    def forward(self, obs):
         out = self.fc1(obs)
         out = self.fc2(out)
         return out
@@ -34,13 +34,13 @@ class Agent(parl.Agent):
         self.pred_program = fluid.Program()
         self.learn_program = fluid.Program()
 
-        with fluid.program_guard(self.pred_program):  # 搭建计算图用于 预测动作，定义输入输出变量
+        with fluid.program_guard(self.pred_program): 
             obs = layers.data(
                 name='obs', shape=[self.obs_dim], dtype='float32')
             self.act_prob = self.alg.predict(obs)
 
         with fluid.program_guard(
-                self.learn_program):  # 搭建计算图用于 更新policy网络，定义输入输出变量
+                self.learn_program): 
             obs = layers.data(
                 name='obs', shape=[self.obs_dim], dtype='float32')
             act = layers.data(name='act',shape=[1], dtype='int64') 
@@ -48,14 +48,14 @@ class Agent(parl.Agent):
             self.cost = self.alg.learn(obs, act, reward)
 
     def sample(self, obs):
-        obs = np.expand_dims(obs, axis=0)  # 增加一维维度
+        obs = np.expand_dims(obs, axis=0)  
         act_prob = self.fluid_executor.run(
             self.pred_program,
             feed={'obs': obs.astype('float32')},
-            fetch_list=[self.act_prob])[0]                                   #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            fetch_list=[self.act_prob])[0]                                  
         act=[0.0,0.0,0.0]
-        act[0] = np.random.choice(np.array([-0.2,0,0.2]), p=(act_prob[0]))  # 根据动作概率选取动作range(self.act_dim)
-        act[1] = np.random.choice(np.array([1,1,2]), p=(act_prob[0]))  # p=(act_prob[0]) p规定选取a中每个元素的概率，默认为概率相同
+        act[0] = np.random.choice(np.array([-0.2,0,0.2]), p=(act_prob[0])) 
+        act[1] = np.random.choice(np.array([1,1,2]), p=(act_prob[0])) 
         act[2] = np.random.choice(np.array([0,0.3,0.7]), p=(act_prob[0]))  # 转向、油门、刹车
         return act
 
@@ -66,12 +66,12 @@ class Agent(parl.Agent):
             feed={'obs': obs.astype('float32')},
             fetch_list=[self.act_prob])[0]
         act_prob = np.squeeze(act_prob, axis=0)
-        act = np.argmax(act_prob)  # 根据动作概率选择概率最高的动作
+        act = np.argmax(act_prob)  
         return act
 
     def learn(self, obs, act, reward):
-        # act = np.expand_dims(act, axis=0)   #(1, 1000, 3)
-        print('learn:act.shape',act.shape)#(1000, 3)
+        # act = np.expand_dims(act, axis=0)
+        print('learn:act.shape',act.shape)
         feed = {
             'obs': obs.astype('float32'),
             'act': act.astype('int64'),
@@ -80,7 +80,7 @@ class Agent(parl.Agent):
         print('learn_self.cost',self.cost)
         cost = self.fluid_executor.run(
             self.learn_program, feed=feed, 
-            fetch_list=[self.cost])[0]  #<<<<<<<<<<<<<<<<<<<<<<<
+            fetch_list=[self.cost])[0]  
         return cost
 
 def run_episode(env, agent):
@@ -91,7 +91,7 @@ def run_episode(env, agent):
     while True:
         # env.render()    
         obs_list.append(obs)
-        action = agent.sample(obs) # 采样动作env.action_space.sample()     <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        action = agent.sample(obs) 
         action_list.append(action)
 
         obs, reward, done, info = env.step(action)
@@ -109,7 +109,6 @@ def evaluate(env, agent, render=False):
         obs = env.reset()
         episode_reward = 0
         while True:
-            obs = preprocess(obs) # from shape (210, 160, 3) to (100800,)
             action = agent.predict(obs) # 选取最优动作
             obs, reward, isOver, _ = env.step(action)
             episode_reward += reward
@@ -136,10 +135,10 @@ def calc_reward_to_go(reward_list, gamma=0.99):
 if __name__ =='__main__':
     # 创建环境
     env = gym.make('CarRacing-v0')
-    obs_dim = 96*96*3 #env.observation_space.shape[0] #96*96*3
+    obs_dim = 96*96*3 
     act_dim = env.action_space.shape[0]  #转向、油门、刹车
     print(act_dim)
-    logger.info('obs_dim {}, act_dim {}'.format(obs_dim, act_dim))   # obs_dim 288, act_dim 3
+    logger.info('obs_dim {}, act_dim {}'.format(obs_dim, act_dim))  
 
     model = Model(act_dim=act_dim)
     alg = PolicyGradient(model, lr=LEARNING_RATE)
@@ -150,7 +149,7 @@ if __name__ =='__main__':
         agent.restore('./model.ckpt')
     for i in range(30):
         print(i)
-        obs_list, action_list, reward_list = run_episode(env, agent)  #<<<<<<<<<<<<<<<<<<<
+        obs_list, action_list, reward_list = run_episode(env, agent)  
         print("Train Episode {}, Reward Sum {}.".format(i, sum(reward_list)))
         if i % 10 == 0:
             logger.info("Train Episode {}, Reward Sum {}.".format(i, sum(reward_list)))
@@ -158,7 +157,7 @@ if __name__ =='__main__':
         batch_action = np.array(action_list)
         batch_reward = calc_reward_to_go(reward_list)
 
-        # agent.learn(batch_obs, batch_action, batch_reward)  #<<<<<<<<<<<<<<<<<<<<<<<
+        # agent.learn(batch_obs, batch_action, batch_reward) 
         if (i + 1) % 100 == 0:
             total_reward = evaluate(env, agent, render=False)
             logger.info('Episode {}, Test reward: {}'.format(i + 1, 
